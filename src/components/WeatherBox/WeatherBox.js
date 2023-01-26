14e00876c8b0936ba49d8d0e96c4006491e69cc7
Pawel Godula <bolomaster7@gmail.com>
2023-01-26 12:53:58 +0100

    render correct image

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,33 +1,32 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 
 const WeatherBox = props => {
-
+  const [weatherData, setWeatherData] = useState(null);
   const handleCityChange = useCallback(city => {
     console.log(city)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=19e6c4cc18b8bf97df00d626957ca779&units=metric`)
-   .then(res => res.json())
-   .then(data => {
-     console.log(data);
-   });
-  }
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=19e6c4cc18b8bf97df00d626957ca779&units=metric`)
+    .then(res => res.json())
+    .then(data => {
+      const weatherData = {
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main
+        };
+        setWeatherData(weatherData);
+    });
+    }
   ); 
-
-  // const weatherData = {
-  //  city: data.name,
-   // temp: data.main.temp,
-    //icon: data.weather[0].icon,
-   // description: data.weather[0].main
-//};
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary />
+      { weatherData && (<WeatherSummary city={weatherData.city} temp={weatherData.temp} icon={weatherData.icon} description={weatherData.description} />) }
       <Loader />
     </section>
   )
